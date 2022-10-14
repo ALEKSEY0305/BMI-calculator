52fc9a3614fe95f1aa0f5dfbd7ee1e7770317001
ALEKSEY KIM <alex.romanovich.ak@gmail.com>
2022-10-15 04:52:30 +0900

    added values to us metrics

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ window.addEventListener('DOMContentLoaded', () => {
     activeForm = 'bmi-usc';
 });
 
+CALC_BTN.addEventListener('click', performBMICalc);
+
 //bmi calculation form toggle
 BMI_HEADS.forEach(bmiHead => {
     bmiHead.addEventListener('click', () => {
@@ -18,17 +20,43 @@ BMI_HEADS.forEach(bmiHead => {
             removeActiveClass();
             bmiHead.classList.add('active-head');
             BMI_SI.classList.remove('show-bmi');
-            BMI_USC.add('show-bmi');
+            BMI_USC.classList.add('show-bmi');
             activeForm = 'bmi-usc';
         }
         if(bmiHead.id === "bmi-si-head"){
             removeActiveClass();
             bmiHead.classList.add('active-head');
             BMI_USC.classList.remove('show-bmi');
-            BMI_SI.add('show-bmi');
+            BMI_SI.classList.add('show-bmi');
             activeForm = 'bmi-si';
         }
     });
 });
 
+// remove active class from heads
+function removeActiveClass(){
+    BMI_HEADS.forEach(bmiHead => {
+        bmiHead.classList.remove('active-head');
+    });
+}
 
+
+//main bmi calculation
+function performBMICalc(){
+    let BMIInfo = getUserInput();
+}
+
+//get input values 
+function getUserInput(){
+    let status;
+    //get input from us units
+    if(activeForm === "bmi-usc"){
+        let age = document.getElementById('age1').value,
+        gender = document.querySelector('#bmi-usc input[name = "gender"]:checked').value,
+        heightFeet = document.getElementById('feet').value,
+        heightInches = documet.getElementById('inches').value,
+        weightPounds = document.getElementById('pounds').value;
+
+        console.log(age, gender,heightFeet, heightInches, weightPounds);
+    }
+}
